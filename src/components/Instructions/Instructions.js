5631f9fa78b0ed825456dd6984c9abf3f2d5705d
Aqil Zeeshan <azeeshan@freewheel.com>
2022-05-13 13:09:18 +0100

    Arrow Functional Component

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -2,12 +2,11 @@ import React from 'react';
 import './Instructions.css';
 import emoji from './emoji.svg'
 
-export default function Instructions() {
-    return(
-        <div className="instructions">
-            <img alt="laughing crying emoji" src={emoji} />
-            <p>Click on an emoji to view the emoji short name.</p>
-        </div>
-    )
-}
+const Instructions = () => (
+    <div className="instructions">
+        <img alt="laughing crying emoji" src={emoji} />
+        <p>Click on an emoji to view the emoji short name.</p>
+    </div>
+)
 
+export default Instructions
