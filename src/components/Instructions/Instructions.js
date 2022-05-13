a29b372aaed20e924559fe7456f6a2d29e8fd521
Aqil Zeeshan <azeeshan@freewheel.com>
2022-05-13 13:02:38 +0100

    Functional Component

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
-import './Instructions.css'
+import React from 'react';
+import './Instructions.css';
 import emoji from './emoji.svg'
 
-export default class Instructions extends Component {
-
-    render() {
-        return(
-            <div className="instructions">
-                <img alt="laughing crying emoji" src={emoji} />
-                <p>Click on an emoji to view the emoji short name.</p>
-            </div>
-        )
-    }
+export default function Instructions() {
+    return(
+        <div className="instructions">
+            <img alt="laughing crying emoji" src={emoji} />
+            <p>Click on an emoji to view the emoji short name.</p>
+        </div>
+    )
 }
 
